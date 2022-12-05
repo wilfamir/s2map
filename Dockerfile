8898fe7ce04c20b049ab04de8f921cca2c3163f8
Amir Wilf <amir@ridewithvia.com>
2022-12-05 14:46:24 +0200

    app binds to 0.0.0.0 if dockerized

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM ubuntu:17.10
+FROM ubuntu:18.04
+
+ARG APP_HOST=0.0.0.0
+ENV APP_HOST=$APP_HOST
+
 RUN apt-get update
-RUN apt-get -y install make g++ swig libssl-dev cmake gawk libevent-dev libcurl4-openssl-dev libboost-dev nginx
+RUN apt-get -y install make g++ swig libssl-dev cmake gawk libevent-dev libcurl4-openssl-dev libboost-dev nginx libxslt-dev
 RUN apt-get -y install curl less vim
 RUN apt-get -y install python-pip
 COPY s2map-server/ /usr/src/myapp/s2map-server
@@ -14,7 +18,5 @@ COPY run-all.sh /usr/src/myapp/
 WORKDIR /usr/src/myapp
 COPY nginx.conf /etc/nginx/sites-enabled/proxy.conf
 RUN rm /etc/nginx/sites-enabled/default
-CMD ["/usr/src/myapp/run-all.sh"]
-# RUN apt-get -y install python python-pip
-#CMD ["python", "-m", "SimpleHTTPServer", "8000"]
-EXPOSE 81
+EXPOSE 5000
+CMD "/usr/src/myapp/run-all.sh"
--- frontend/app.py
+++ frontend/app.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask
-from flask import Response
-from flask import stream_with_context
-from flask import Flask, request, send_from_directory, render_template
-from flask import jsonify
-
-import math
 import logging
+import s2sphere
+import os
+from flask import Flask, send_from_directory
+from flask import jsonify
+from flask import make_response
+from functools import update_wrapper
+from datetime import datetime
+from functools import wraps
+from flask import request, current_app
+
+
 logging.basicConfig()
 
-from flask import make_response
-from functools import wraps, update_wrapper
-from datetime import datetime
-
-import s2sphere
 
 def nocache(view):
     @wraps(view)
@@ -31,35 +29,33 @@ def nocache(view):
 
 app = Flask(__name__)
 
+
 def cell_id_to_json(cellid_l):
-  cellid = s2sphere.CellId(cellid_l)
-  cell = s2sphere.Cell(cellid)
+    cellid = s2sphere.CellId(cellid_l)
+    cell = s2sphere.Cell(cellid)
 
-  def get_vertex(v):
-    vertex = s2sphere.LatLng.from_point(cell.get_vertex(v))
+    def get_vertex(v):
+        vertex = s2sphere.LatLng.from_point(cell.get_vertex(v))
 
+        return {
+            'lat': vertex.lat().degrees,
+            'lng': vertex.lng().degrees
+        }
+
+    shape = [get_vertex(v) for v in range(0, 4)]
     return {
-      'lat': vertex.lat().degrees,
-      'lng': vertex.lng().degrees
+        'id': str(cellid.id()),
+        'id_signed': cellid.id(),
+        'token': cellid.to_token(),
+        'pos': cellid.pos(),
+        'face': cellid.face(),
+        'level': cellid.level(),
+        'll': {
+            'lat': cellid.to_lat_lng().lat().degrees,
+            'lng': cellid.to_lat_lng().lng().degrees
+        },
+        'shape': shape
     }
-
-  shape = [get_vertex(v) for v in range(0, 4)]
-  return {
-    'id': str(cellid.id()),
-    'id_signed': cellid.id(),
-    'token': cellid.to_token(),
-    'pos': cellid.pos(),
-    'face': cellid.face(),
-    'level': cellid.level(),
-    'll': {
-      'lat': cellid.to_lat_lng().lat().degrees,
-      'lng': cellid.to_lat_lng().lng().degrees
-    },
-    'shape': shape
-  }
-
-from functools import wraps
-from flask import request, current_app
 
 
 def jsonp(func):
@@ -76,29 +72,34 @@ def jsonp(func):
             return func(*args, **kwargs)
     return decorated_function
 
+
 @app.route('/api/s2info', methods=['GET', 'POST'])
 @jsonp
 def s2info():
-  # need to make this work for GET and POST
-  ids = request.args.get("id") or request.form['id'] or ''
-  return jsonify([cell_id_to_json(int(id)) for id in ids.split(',')])
+    # need to make this work for GET and POST
+    ids = request.args.get("id") or request.form['id'] or ''
+    return jsonify([cell_id_to_json(int(id)) for id in ids.split(',')])
+
 
 @nocache
 @app.route('/<path:path>')
 def send_app(path):
- print('trying to send ' + path)
- return send_from_directory('.', path)
+    print('trying to send ' + path)
+    return send_from_directory('.', path)
+
 
 @nocache
 @app.route('/')
 def send_index():
-  print('sending index?')
-  return send_from_directory('.', "index.html")
+    print('sending index?')
+    return send_from_directory('.', "index.html")
+
 
 @nocache
 @app.route('/ind')
 def send_i2ndex():
-  return send_from_directory('.', "index.html")
+    return send_from_directory('.', "index.html")
+
 
 if __name__ == '__main__':
-  app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, host=os.getenv("APP_HOST", "127.0.0.1"))
